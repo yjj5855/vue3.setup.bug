ad1b9b619072dbfc656a4a823823610bf1d23556
yjj <651380735@qq.com>
2021-12-25 11:35:10 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref, reactive} from 'vue'
+const ref_form = ref(null)
+const form = reactive({name: '', password: ''})
+</script>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.ref_form)
+    console.log(this.form)
+  }
+}
+</script>
+
+<template>
+  <img ref="ref_form" alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
